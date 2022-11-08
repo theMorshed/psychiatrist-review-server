24b4001954248d38e85efbc4de9b849ffba5b911
theMorshed <themorshedctg@gmail.com>
2022-11-09 00:44:23 +0600

    create all services get api

--- index.js
+++ index.js
@@ -16,11 +16,25 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('psychiatrist').collection('services');
-        app.post('/service/add', async(req, res) => {
+        app.post('/service/add', async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
             res.send(result);
-        })
+        });
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+
+        app.get('/all-services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
     }
     finally {
 
