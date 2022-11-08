1e701b073165ae50da0c749cea20ad474ee194c2
theMorshed <themorshedctg@gmail.com>
2022-11-09 00:07:46 +0600

    install mongodb, connect server with mongodb atlas and create insert service api

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// mongodb driver code
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zkjorm4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('psychiatrist').collection('services');
+        app.post('/service/add', async(req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Hello from psychologist server..');
