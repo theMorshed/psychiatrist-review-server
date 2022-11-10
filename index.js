66f40e90b813dafc638ba94070f84889a6c26626
theMorshed <themorshedctg@gmail.com>
2022-11-10 16:22:35 +0600

    implement jwt token and safe user data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express();
 require('dotenv').config();
@@ -8,6 +9,24 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 // mongodb driver code
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zkjorm4.mongodb.net/?retryWrites=true&w=majority`;
@@ -18,6 +37,12 @@ async function run() {
         
         const serviceCollection = client.db('psychiatrist').collection('services');
         const reviewCollection = client.db('psychiatrist').collection('reviews');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ token });
+        }); 
 
         app.post('/service/add', async (req, res) => {
             const service = req.body;
@@ -46,7 +71,12 @@ async function run() {
             res.send(service);
         });
 
-        app.get('/reviews/:email', async (req, res) => {
+        app.get('/reviews/:email', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
+            }
+
             const email = req.params.email;
             const query = { email: email };
             const reviews = await reviewCollection.find(query).toArray();
